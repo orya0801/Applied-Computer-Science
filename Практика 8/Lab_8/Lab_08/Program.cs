ee3d5226bee215b33989fec724b8e1957a52e544
Orya <oreldan08@gmail.com>
2020-05-04 16:29:31 +0300

    2/3 done

--- Практика 8/Lab_8/Lab_08/Program.cs
+++ Практика 8/Lab_8/Lab_08/Program.cs
@@ -88,24 +88,23 @@ namespace Lab_08
 
             #region Zadanie 2
 
-            Expr expr_f1 = 2 * x * x + 2 * y1 * y1;
+            Expr expr_f1 = 2 * x * x + 2 * y1 + y2;
             Func<double, double, double, double> f1 = (expr_f1).Compile("x", "y1", "y2");
             Expr expr_f2 = 1 - 2 * x * x + 2 * y1 - y2;
             Func<double, double, double, double> f2 = (expr_f2).Compile("x", "y1", "y2");
 
-            Miln(x, y1, y2, f1, f2, 10, 0, 1, 0, 1);
+            Miln(f1, f2, 10, 0, 1, 0, 0);
 
             #endregion
         }
 
-        private static void Miln(Expr x, Expr y1, Expr y2, 
-            Func<double, double, double, double> f1, Func<double, double, double, double> f2,
+        private static void Miln(Func<double, double, double, double> f1, Func<double, double, double, double> f2,
             int divisions, double start_point, double end_point, double y1_start, double y2_start)
         {
             Console.WriteLine("Метод Милна:");
             double k1, l1, k2, l2, k3, l3, k4, l4;
 
-            double h = (start_point - end_point) / divisions;
+            double h = (end_point - start_point) / divisions;
 
             double curr_x = start_point;
 
@@ -115,8 +114,8 @@ namespace Lab_08
             double[] dy1 = new double[divisions + 1];
             double[] dy2 = new double[divisions + 1];
             x_val[0] = start_point;
-            y1_val[0] = y1_start;
-            y2_val[0] = y2_start;
+            dy1[0] = y1_val[0] = y1_start;
+            dy2[0] = y2_val[0] = y2_start;
 
             // Решение системы уравнений методом Рунге-Кутта
             for(int i = 0; i <= 3; i++)
@@ -130,50 +129,54 @@ namespace Lab_08
                 k4 = h * f1(x_val[0] + h, dy1[i] + k3, dy2[i] + l3);
                 l4 = h * f2(x_val[0] + h, dy1[i] + k3, dy2[i] + l3);
 
-                dy1[i + 1] = dy1[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-                dy2[i + 1] = dy2[i] + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
+                dy1[i + 1] = y1_val[i + 1] = dy1[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+                dy2[i + 1] = y2_val[i + 1] = dy2[i] + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
 
                 x_val[i + 1] = x_val[i] + h;
             }
 
-            double[] y1_pr, y1_kor, y2_pr, y2_kor;
+            double curr_y1, curr_y2;
             double dy1_1, dy2_1, dy1_2, dy2_2;
 
-            y1_pr = y1_kor = y2_pr = y2_kor = new double[divisions + 1];
             
 
-            for(int i = 3; i <= divisions; i++)
+            for(int i = 3; i < divisions; i++)
             {
                 x_val[i + 1] = x_val[i] + h;
 
-                y1_pr[i + 1] = y1_val[i - 3] + (4 * h) / 3 * (2 * f1(x_val[i - 2], y1_val[i - 2], y2_val[i - 2]) - f1(x_val[i - 1], y1_val[i - 1], y2_val[i - 1]) + 2 * f1(x_val[i], y1_val[i], y2_val[i]));
+                curr_y1 = y1_val[i - 3] + (4 * h) / 3 * (2 * dy1[i] - dy1[i - 1] + 2 * dy1[i - 2]);
+                curr_y2 = y2_val[i - 3] + (4 * h) / 3 * (2 * dy2[i] - dy2[i - 1] + 2 * dy2[i - 2]);      
 
-                y2_pr[i + 1] = y2_val[i - 3] + (4 * h) / 3 * (2 * f2(x_val[i - 2], y1_val[i - 2], y2_val[i - 2]) - f2(x_val[i - 1], y1_val[i - 1], y2_val[i - 1]) + 2 * f2(x_val[i], y1_val[i], y2_val[i]));
-              
+                dy1_1 = f1(x_val[i + 1], curr_y1, curr_y2);
+                dy2_1 = f2(x_val[i + 1], curr_y1, curr_y2);
 
-                dy1_1 = f1(x_val[i + 1], y1_pr[i + 1], y2_pr[i + 1]);
-                dy2_1 = f2(x_val[i + 1], y1_pr[i + 1], y2_pr[i + 1]);
+                int count = 0;
 
                 while (true)
                 {
-                    y1_kor[i + 1] = y1_val[i - 1] + (h / 3) * (dy1_1 + 4 * f1(x_val[i], y1_val[i], y2_val[i]) + f1(x_val[i - 1], y1_pr[i - 1], y2_val[i - 1]));
-                    y2_kor[i + 1] = y2_val[i - 1] + (h / 3) * (dy2_1 + 4 * f2(x_val[i], y1_val[i], y2_val[i]) + f2(x_val[i - 1], y1_pr[i - 1], y2_val[i - 1]));
-                    dy1_2 = f1(x_val[i + 1], y1_kor[i], y2_kor[i]);
-                    dy2_2 = f1(x_val[i + 1], y1_kor[i], y2_kor[i]);
-                    if (Math.Abs(dy2_2 - dy2_1) < 1e-3 && Math.Abs(dy1_2 - dy1_1) < 1e-3) break;
+                    curr_y1 = y1_val[i - 1] + (h / 3) * (dy1_1 + 4 * dy1[i] + dy1[i - 1]);
+                    curr_y2 = y2_val[i - 1] + (h / 3) * (dy2_1 + 4 * dy2[i] + dy2[i - 1]);
+                    dy1_2 = f1(x_val[i + 1], curr_y1, curr_y2);
+                    dy2_2 = f2(x_val[i + 1], curr_y1, curr_y2);
+                    if (Math.Abs(dy2_2 - dy2_1) < 1e-3 && Math.Abs(dy1_2 - dy1_1) < 1e-3
+                        || count >= 20) break;
                     dy1_1 = dy1_2;
                     dy2_1 = dy2_2;
+                    count++;
                 }
 
-                y1_val[i + 1] = y1_val[i - 2] + (h / 3) * (f1(x_val[i - 2], y1_val[i - 2], y2_val[i - 2]) + 4 * f1(x_val[i - 1], y1_val[i - 1], y2_val[i - 1]) + f1(x_val[i], y1_pr[i], y2_val[i]));
-                y2_val[i + 1] = y2_val[i - 2] + (h / 3) * (f1(x_val[i - 2], y1_val[i - 2], y2_val[i - 2]) + 4 * f1(x_val[i - 1], y1_val[i - 1], y2_val[i - 1]) + f1(x_val[i], y1_pr[i], y2_val[i]));
+                dy1[i + 1] = dy1_2;
+                dy2[i + 1] = dy2_2;
+
+                y1_val[i + 1] = y1_val[i - 1] + (h / 3) * (dy1[i + 1] + 4 * dy1[i] + dy1[i - 1]);
+                y2_val[i + 1] = y2_val[i - 1] + (h / 3) * (dy2[i + 1] + 4 * dy2[i] + dy2[i - 1]);
             }
 
+            Console.WriteLine("x\ty1\ty2");
             for (int i = 0; i < 11; i++)
             {
-                Console.WriteLine($"x={x[i]}    y1={y1[i]}  y2={y2[i]}");
+                Console.WriteLine($"{x_val[i]:0.00}\t{y1_val[i]:0.00}\t{y2_val[i]:0.00}");
             }
-
         }
     }
 }
