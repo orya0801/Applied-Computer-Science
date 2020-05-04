f6c4bd6c5886d505368008b3b51c7f8326909d91
Orya <oreldan08@gmail.com>
2020-05-04 18:39:08 +0300

    Miln still doesn't work properly

--- Практика 8/Lab_8/Lab_08/Program.cs
+++ Практика 8/Lab_8/Lab_08/Program.cs
@@ -69,7 +69,7 @@ namespace Lab_08
             Console.WriteLine($"y2_2 = 1 + integrate (x^2 + (1 + x^2 + x^4/4))dx from 0 to x = {expr_y2_2.ToString()}\n");
 
             Console.WriteLine("Геометрически последовательные приближения представляют " +
-                "собой кривые yn = yn(x)(n = 1, 2, …),проходящие через общую точку M0(x0, y0)." +
+                "собой кривые yn = yn(x)(n = 1, 2, ...),проходящие через общую точку M0(x0, y0)." +
                 "\nВ таблице приведены кривые для 1-ого и 2-ого уравнения системы: \n");
 
             //Результат
@@ -84,24 +84,98 @@ namespace Lab_08
                 Console.WriteLine($"{y2_2(x_val[i]):0.0000}\t");
             }
 
+            Console.WriteLine();
+
             #endregion
 
             #region Zadanie 2
 
-            Expr expr_f1 = 2 * x * x + 2 * y1 + y2;
+            Expr expr_f1 = - 2 * y1 + 4 * y2;
             Func<double, double, double, double> f1 = (expr_f1).Compile("x", "y1", "y2");
-            Expr expr_f2 = 1 - 2 * x * x + 2 * y1 - y2;
+            Expr expr_f2 = - y1 + 3 * y2;
             Func<double, double, double, double> f2 = (expr_f2).Compile("x", "y1", "y2");
 
-            Miln(f1, f2, 10, 0, 1, 0, 0);
+            Console.WriteLine($"Уравнение:\ny1' = {expr_f1.ToString()}\ny2' = {expr_f2.ToString()}\n");
+
+            Miln(f1, f2, 10, 0, 1, 3, 0);
+
+            #endregion
+
+
+            #region Zadanie 3
+
+            Adams(f1, f2, 10, 0, 1, 3, 0);
 
             #endregion
         }
 
+        private static void Adams(Func<double, double, double, double> f1, Func<double, double, double, double> f2,
+            int divisions, double start_point, double end_point,  double y1_start, double y2_start)
+        {
+            Console.WriteLine("Метод Адамса:\n");
+
+            double h = (end_point - start_point) / divisions;
+
+            double k1, k2, k3, k4, l1, l2, l3, l4;
+
+            double[] y1, y2, x;
+
+            x = new double[divisions + 1];
+            y1 = new double[divisions + 1];
+            y2 = new double[divisions + 1];
+
+            x[0] = start_point;
+            y1[0] = y1_start;
+            y2[0] = y2_start;
+
+
+            for(int i = 0; i < 4; i++)
+            {
+                    k1 = h * f1(x[i], y1[i], y2[i]);
+                    l1 = h * f2(x[i], y1[i], y2[i]);
+                    k2 = h * f1(x[i] + h / 2, y1[i] + k1 / 2, y2[i] + l1 / 2);
+                    l2 = h * f2(x[i] + h / 2, y1[i] + k1 / 2, y2[i] + l1 / 2);
+                    k3 = h * f1(x[i] + h / 2, y1[i] + k2 / 2, y2[i] + l2 / 2);
+                    l3 = h * f2(x[i] + h / 2, y1[i] + k2 / 2, y2[i] + l2 / 2);
+                    k4 = h * f1(x[i] + h, y1[i] + k3, y2[i] + l3);
+                    l4 = h * f2(x[i] + h, y1[i] + k3, y2[i] + l3);
+
+                    y1[i + 1] = y1[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+                    y2[i + 1] = y2[i] + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
+
+                    x[i + 1] = x[i] + h;
+            }
+
+            for (int i = 4; i <= divisions; i++)
+            {
+                x[i] = x[i - 1] + h;
+
+                y1[i] = y1[i - 1] + (h / 24.0) *
+                            (55 * f1(x[i - 1], y1[i - 1], y2[i - 1]) -
+                             59 * f1(x[i - 2], y1[i - 2], y2[i - 2]) +
+                             37 * f1(x[i - 3], y1[i - 3], y2[i - 3]) +
+                             -9 * f1(x[i - 4], y1[i - 4], y2[i - 4]));
+
+                y2[i] = y2[i - 1] + (h / 24.0) *
+                            (55 * f2(x[i - 1], y1[i - 1], y2[i - 1]) -
+                             59 * f2(x[i - 2], y1[i - 2], y2[i - 2]) +
+                             37 * f2(x[i - 3], y1[i - 3], y2[i - 3]) +
+                             -9 * f2(x[i - 4], y1[i - 4], y2[i - 4]));
+            }
+
+            //Вывод ответа
+            Console.WriteLine($"x\ty1\ty2");
+            for (int i = 0; i <= divisions; i++)
+            {
+                Console.WriteLine($"{x[i]:0.00}\t{y1[i]:0.00}\t{y2[i]:0.00}");
+            }
+        }
+
+
         private static void Miln(Func<double, double, double, double> f1, Func<double, double, double, double> f2,
             int divisions, double start_point, double end_point, double y1_start, double y2_start)
         {
-            Console.WriteLine("Метод Милна:");
+            Console.WriteLine("Метод Милна:\n");
             double k1, l1, k2, l2, k3, l3, k4, l4;
 
             double h = (end_point - start_point) / divisions;
@@ -120,14 +194,14 @@ namespace Lab_08
             // Решение системы уравнений методом Рунге-Кутта
             for(int i = 0; i <= 3; i++)
             {
-                k1 = h * f1(x_val[0], dy1[0], dy2[0]);
-                l1 = h * f2(x_val[0], dy1[0], dy2[0]);
-                k2 = h * f1(x_val[0] + h / 2, dy1[0] + k1 / 2, dy2[i] + l1 / 2);
-                l2 = h * f2(x_val[0] + h / 2, dy1[0] + k1 / 2, dy2[i] + l1 / 2);
-                k3 = h * f1(x_val[0] + h / 2, dy1[i] + k2 / 2, dy2[i] + l2 / 2);
-                l3 = h * f2(x_val[0] + h / 2, dy1[i] + k2 / 2, dy2[i] + l2 / 2);
-                k4 = h * f1(x_val[0] + h, dy1[i] + k3, dy2[i] + l3);
-                l4 = h * f2(x_val[0] + h, dy1[i] + k3, dy2[i] + l3);
+                k1 = h * f1(x_val[i], dy1[i], dy2[i]);
+                l1 = h * f2(x_val[i], dy1[i], dy2[i]);
+                k2 = h * f1(x_val[i] + h / 2, dy1[i] + k1 / 2, dy2[i] + l1 / 2);
+                l2 = h * f2(x_val[i] + h / 2, dy1[i] + k1 / 2, dy2[i] + l1 / 2);
+                k3 = h * f1(x_val[i] + h / 2, dy1[i] + k2 / 2, dy2[i] + l2 / 2);
+                l3 = h * f2(x_val[i] + h / 2, dy1[i] + k2 / 2, dy2[i] + l2 / 2);
+                k4 = h * f1(x_val[i] + h, dy1[i] + k3, dy2[i] + l3);
+                l4 = h * f2(x_val[i] + h, dy1[i] + k3, dy2[i] + l3);
 
                 dy1[i + 1] = y1_val[i + 1] = dy1[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
                 dy2[i + 1] = y2_val[i + 1] = dy2[i] + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
@@ -158,8 +232,7 @@ namespace Lab_08
                     curr_y2 = y2_val[i - 1] + (h / 3) * (dy2_1 + 4 * dy2[i] + dy2[i - 1]);
                     dy1_2 = f1(x_val[i + 1], curr_y1, curr_y2);
                     dy2_2 = f2(x_val[i + 1], curr_y1, curr_y2);
-                    if (Math.Abs(dy2_2 - dy2_1) < 1e-3 && Math.Abs(dy1_2 - dy1_1) < 1e-3
-                        || count >= 20) break;
+                    if (Math.Abs(dy2_2 - dy2_1) < 1e-4 && Math.Abs(dy1_2 - dy1_1) < 1e-4) break;
                     dy1_1 = dy1_2;
                     dy2_1 = dy2_2;
                     count++;
@@ -177,6 +250,8 @@ namespace Lab_08
             {
                 Console.WriteLine($"{x_val[i]:0.00}\t{y1_val[i]:0.00}\t{y2_val[i]:0.00}");
             }
+
+            Console.WriteLine();
         }
     }
 }
